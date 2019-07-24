f0b0cf8119f1f4e6d9e669984386ff1f219dea1b
ruslancik <44032621+ruslancik@users.noreply.github.com>
2019-07-25 01:22:30 +0300

    home heading part 2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 var element = document.querySelectorAll('.anime').forEach(function(el){
-    el.addEventListener('mouseenter', function(e){
+    el.addEventListener('mouseover', function(e){
         e.target.classList.add('animated', 'rubberBand');
         e.target.style='color:#08fdd8';
+
+        setTimeout(() => {
+            e.target.classList.remove('animated', 'rubberBand')
+        }, 1000);
+
         e.preventDefault();
-    })
+     })
 })
 
-function removeAnime(){
-    document.querySelectorAll('.anime').forEach(function(e){
-        e.classList.remove('animated', 'rubberBand');
-    })
-}
-removeAnime();
+var element = document.querySelectorAll('.anime').forEach(function(el){
+    el.addEventListener('mouseout', function(e){
+        e.target.style='color:#fff';
+        e.preventDefault();
+    })  
+})
+
